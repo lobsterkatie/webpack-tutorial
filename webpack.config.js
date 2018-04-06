14599befade6d9039d58a21707f3ceaebe400fa7
Katie Byers <lobsterkatie@gmail.com>
2018-04-05 22:09:09 -0700

    first commit; got react working in static file

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+
+const config = {
+    entry: __dirname + '/static/js/index.jsx',
+    output: {
+        path: __dirname + '/static/dist',
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: __dirname + '/node_modules/',
+                // include: __dirname + '/static',
+                use: 'babel-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+};
+
+module.exports = config;
